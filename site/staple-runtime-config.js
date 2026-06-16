1ea8ecdfdf50bef65adce8854c5d180a3178484b
shlee <shlee@shleeui-MacStudio.local>
2026-06-16 22:29:03 +0900

    Restore STAPLE app Pages build

---
+++ site/staple-runtime-config.js
@@ -0,0 +1,29 @@
+window.STAPLE_DEMO_CONFIG = {
+  demoRpcUrl: "https://rpc.nobaprotocol.com",
+  faucetApiUrl: "https://faucet.nobaprotocol.com",
+  demoChainId: 5745438,
+  demoNetworkName: "STAPLE Public Demo",
+  demoSymbol: "ETH",
+  demoExplorerUrl: "",
+  walletConnectProjectId: "YOUR_PROJECT_ID",
+  contracts: {
+    STAPLE_ADDRESS: "0x6d4923d4873aaBCcF23d4585b127a0eFF1381C91",
+    ORACLE: "0x68cAd734a0ff70C35bEB2B6D76aB329B82BE8A30",
+    CONTRACT_ADDRESS: "0x848DEfD2C16C90562D0BDFc0b41492455b9a178c",
+    ATM: "0xdab7C577DfD6dD22Ff2d23357e27D53CD53D5cBD",
+    ADAPTER: "0xEb74f01591173b3D68b656dFD28E32246C39ea38",
+    QUEUE: "0x15dFab5640F2d219D38817fa8234DA344C0b7Fa4",
+    LENS: "0x16Ee9B99AB0322a958dDda3e7829c665b6F6327A",
+    REBALANCER: "0xb6CC00B69bff1D88747A15aB39BF811ED7118f17",
+    TOKEN_ADDRESSES: {
+      usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    },
+    TOKEN_DECIMALS: {
+      usdc: 6,
+      usdt: 6,
+      dai: 18
+    }
+  }
+};
